409e5c2356566caf38c2320758a444f36181890b
Tyler Atkinson <tylerh.atkinson@gmail.com>
2018-10-05 14:41:31 -0400

    Begun work training the network, also added cleanup for dynamic variables

--- src/Network.c
+++ src/Network.c
@@ -12,14 +12,14 @@ void initializeNetwork(Network *network, int inputs, int hidden, int outputs) {
     network->hiddenNodes = (Node*)malloc(sizeof(Node) * hidden);
     network->outputNodes = (Node*)malloc(sizeof(Node) * outputs);
     network->weightMap = (HashTable*)malloc(sizeof(HashTable));
-    network->bias.name = "_BIAS";
+    network->bias.name = "BIAS";
     network->bias.data = 1.0;
     network->inputSize = inputs;
     network->hiddenSize = hidden;
     network->outputSize = outputs;
     int size = ((inputs * hidden) + hidden + outputs + (hidden * outputs)) * 2;
     initializeHashTable(network->weightMap, size);
-
+    //Initializing input nodes
     for(int i = 0; i < inputs; i++) {
         Node node;
         char *name = (char*)malloc(sizeof(char) * 10);
@@ -27,6 +27,7 @@ void initializeNetwork(Network *network, int inputs, int hidden, int outputs) {
         node.name = name;
         network->inputNodes[i] = node;
     }
+    //Initializing hidden nodes and weights
     for(int i = 0; i < hidden; i++) {
         Node node;
         char *name = (char*)malloc(sizeof(char) * 15);
@@ -37,12 +38,14 @@ void initializeNetwork(Network *network, int inputs, int hidden, int outputs) {
         sprintf(biasName, "%s->%s", name, network->bias.name);
         insert(network->weightMap, biasName, getRandomNumber());
         network->hiddenNodes[i] = node;
+        //Initializing weights from input -> hidden
         for(int y = 0; y < inputs; y++) {
             char *weightName = (char*)malloc(sizeof(char) * 22);
             sprintf(weightName, "%s->%s", network->inputNodes[y].name, name);
             insert(network->weightMap, weightName, getRandomNumber());
         }
     }
+    //Initializing output nodes and weights
     for(int i = 0; i < outputs; i++) {
         Node node;
         char *name = (char*)malloc(sizeof(char) * 15);
@@ -53,6 +56,7 @@ void initializeNetwork(Network *network, int inputs, int hidden, int outputs) {
         sprintf(biasName, "%s->%s", name, network->bias.name);
         insert(network->weightMap, biasName, getRandomNumber());
         network->outputNodes[i] = node;
+        //Initializing weights from hidden -> output
         for(int y = 0; y < hidden; y++) {
             char *weightName = (char*)malloc(sizeof(char) * 22);
             sprintf(weightName, "%s->%s", network->hiddenNodes[y].name, name);
@@ -60,10 +64,85 @@ void initializeNetwork(Network *network, int inputs, int hidden, int outputs) {
         }
     }
 }
+void train(Network *network, float **trainingInputs, float **trainingOutputs, float targetError) {
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 2; j++) {
+            printf("%.1f   ", trainingInputs[i][j]);
+        }
+        printf("\n");
+    }
+    float error = 999.9;
+    int numInputs = sizeof(trainingInputs) / network->inputSize;
+    int numOutputs = sizeof(trainingOutputs) / network->outputSize;
+    float *inputs;
+    float *outputs;
+    while(error >= targetError) {
+        int i = rand() % numInputs;
+        inputs = trainingInputs[i];
+        outputs = trainingOutputs[i];
+        
+        return;
+    }
+}
+void cleanup(Network *network) {
+    //Input Nodes
+    for(int i = 0; i < network->inputSize; i++)
+        free(network->inputNodes[i].name);  
+    free(network->inputNodes);
+
+    //HiddenNodes
+    for(int i = 0; i < network->hiddenSize; i++)
+        free(network->hiddenNodes[i].name); 
+    free(network->hiddenNodes);
+
+    //OutputNodes
+    for(int i = 0; i < network->outputSize; i++)
+        free(network->outputNodes[i].name); 
+    free(network->outputNodes);
+
+    //WeightMap
+    deleteAll(network->weightMap);
+    free(network->weightMap->elements);
+    free(network->weightMap);
+    //Network
+    free(network);
+}
 
 int main() {
     srand(time(0));
+    //Initializing network
     Network *network = (Network*)malloc(sizeof(Network));
     initializeNetwork(network, 2, 3, 1);
+    //Training
+    float targetError = 0.00001;
+    float ** trainingInputs = (float**)malloc(sizeof(float*) * 4);
+    float ** trainingOutputs = (float**)malloc(sizeof(float*) * 4);
+    for(int i = 0; i < 4; i++) {
+        trainingInputs[i] = (float*)malloc(sizeof(float) * 2);
+        trainingOutputs[i] = (float*)malloc(sizeof(float) * 1);
+    }
+    trainingInputs[0][0] = 0.1;
+    trainingInputs[0][1] = 0.1;
+    trainingOutputs[0][0] = 0.1;
 
+    trainingInputs[1][0] = 0.1;
+    trainingInputs[1][1] = 0.9;
+    trainingOutputs[1][0] = 0.9;
+
+    trainingInputs[2][0] = 0.9;
+    trainingInputs[2][1] = 0.1;
+    trainingOutputs[2][0] = 0.9;
+
+    trainingInputs[3][0] = 0.9;
+    trainingInputs[3][1] = 0.9;
+    trainingOutputs[3][0] = 0.1;
+    train(network, (float**)trainingInputs, (float**)trainingOutputs, targetError);
+    //Cleanup
+    cleanup(network);
+    for(int i = 0; i < 4; i++) {
+        free(trainingInputs[i]);
+        free(trainingOutputs[i]);
+    }
+    free(trainingInputs);
+    free(trainingOutputs);
 }
